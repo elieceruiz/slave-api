bfa124ce7edc181e8daff611a0e39dae83e60bd9
Eliecer Andrei Ruiz Higuita <elieceruiz@me.com>
2026-06-08 22:37:40 -0500

    connect vercel to render pipeline

---
+++ api/trigger.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://slave-api.onrender.com/run", {
+      method: "POST"
+    });
+
+    const text = await response.text();
+
+    return res.status(200).json({
+      status: "ok",
+      render_status: response.status,
+      render_response: text
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message
+    });
+  }
+}
